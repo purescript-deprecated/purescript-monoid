2d9bd077a140fff127dca0c7d0d09bc12881201a
Phil Freeman <paf31@cantab.net>
2014-03-23 19:22:27 -0700

    First monoid

---
+++ src/Data/Monoid/First.purs
@@ -0,0 +1,17 @@
+module Data.Monoid.First where
+
+import Data.Maybe
+import Data.Monoid
+
+data First a = First (Maybe a)
+
+runFirst :: forall a. First a -> Maybe a
+runFirst (First m) = m
+
+instance showFirst :: (Show a) => Show (First a) where
+  show (First a) = "First (" ++ show a ++ ")"
+
+instance monoidFirst :: Monoid (First a) where
+  mempty = First Nothing
+  (<>) first@(First (Just _)) _ = first
+  (<>) _ second = second
