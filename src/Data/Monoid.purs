f4ac8d4f30647c086de9a947587339f2a6cdd773
Gary Burgess <gary.burgess@gmail.com>
2014-04-25 23:09:43 +0100

    Add Array instance

--- src/Data/Monoid.purs
+++ src/Data/Monoid.purs
@@ -1,7 +1,12 @@
 module Data.Monoid where
 
+import Data.Array ()
+
 class (Semigroup m) <= Monoid m where
   mempty :: m
 
 instance monoidString :: Monoid String where
   mempty = ""
+
+instance monoidArray :: Monoid [a] where
+  mempty = []
