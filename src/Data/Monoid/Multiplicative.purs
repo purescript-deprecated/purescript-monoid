dee2b75b70ab709265725a96178fc7ad66336387
Gary Burgess <gary.burgess@gmail.com>
2015-02-16 23:36:00 +0000

    Add Multiplicative

---
+++ src/Data/Monoid/Multiplicative.purs
@@ -0,0 +1,38 @@
+module Data.Monoid.Multiplicative where
+
+import Data.Monoid
+
+newtype Multiplicative a = Multiplicative a
+
+runMultiplicative :: forall a. Multiplicative a -> a
+runMultiplicative (Multiplicative x) = x
+
+instance eqMultiplicative :: (Eq a) => Eq (Multiplicative a) where
+  (==) (Multiplicative x) (Multiplicative y) = x == y
+  (/=) (Multiplicative x) (Multiplicative y) = x /= y
+
+instance ordMultiplicative :: (Ord a) => Ord (Multiplicative a) where
+  compare (Multiplicative x) (Multiplicative y) = compare x y
+
+instance functorMultiplicative :: Functor Multiplicative where
+  (<$>) f (Multiplicative x) = Multiplicative (f x)
+
+instance applicativeMultiplicative :: Applicative Multiplicative where
+  pure x = Multiplicative x
+
+instance bindMultiplicative :: Bind Multiplicative where
+  (>>=) (Multiplicative x) f = f x
+
+instance monadMultiplicative :: Monad Multiplicative
+
+instance applyMultiplicative :: Apply Multiplicative where
+  (<*>) (Multiplicative f) (Multiplicative x) = Multiplicative (f x)
+
+instance showMultiplicative :: (Show a) => Show (Multiplicative a) where
+  show (Multiplicative a) = "Multiplicative (" ++ show a ++ ")"
+
+instance semigroupMultiplicative :: (Semiring a) => Semigroup (Multiplicative a) where
+  (<>) (Multiplicative a) (Multiplicative b) = Multiplicative (a * b)
+
+instance monoidMultiplicative :: (Semiring a) => Monoid (Multiplicative a) where
+  mempty = Multiplicative one
